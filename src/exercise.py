032f922caad58187240d5855c0773d793884ecf0
Rune Petersen <58623004+runembp@users.noreply.github.com>
2021-08-22 20:05:36 +0000

    1

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    n1 = int(input("Give the first number:"))
+    n2 = int(input("Give the second number:"))
+    sum = n1 * n2
+    print(str(n1) + " * " + str(n2) + " = " + str(sum))
 
 if __name__ == '__main__':
     main()
